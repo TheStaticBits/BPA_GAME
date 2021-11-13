49b3edf4f8938c269bed93d394700f8e799fd3c4
TheStaticBits <TheStaticBits@users.noreply.github.com>
2021-11-13 06:45:46 -0500

    Fixed a small issue

--- src/player.py
+++ src/player.py
@@ -16,6 +16,7 @@ class Player(src.object_base.ObjectBase):
     
 
     def update(self, room, inputs):
+        super().test_grav_line()
         super().update_gravity()
 
         # This will result in a 0, a 1, or a -1. The inputs are True or False.
@@ -26,7 +27,7 @@ class Player(src.object_base.ObjectBase):
         super().update_x_collision(room, inputs["right"] - inputs["left"])
         
         # Update velocity based on inputs
-        if (self.gravityDir == 1 and self.collisions["down"]) or self.collisions["up"]:
+        if (self.gravityDir == 1 and self.collisions["down"]) or (self.gravityDir == -1 and self.collisions["up"]):
             self.canJump = True
             self.yVelocity = 0
         
@@ -36,19 +37,17 @@ class Player(src.object_base.ObjectBase):
             self.yVelocity = constants.JUMP_FORCE * self.gravityDir
 
         # If the player hit the ceiling reset y velocity
-        elif self.gravityDir == 1 and self.collisions["up"] or self.collisions["down"]:
+        elif (self.gravityDir == 1 and self.collisions["up"]) or (self.gravityDir == -1 and self.collisions["down"]):
             if (self.gravityDir == 1 and self.yVelocity > 0) or self.yVelocity < 0:
                 self.yVelocity = 0
         
-        if self.gravityDir == 1 and math.ceil(self.yVelocity) < -1 or math.floor(self.yVelocity) > 1:
+        if (self.gravityDir == 1 and math.ceil(self.yVelocity) < -1) or (self.gravityDir == -1 and math.floor(self.yVelocity) > 1):
             self.canJump = False
 
         super().update_y_pos()
         
         super().reset_current_tile()
         super().update_y_collision(room, 1 if self.yVelocity > 0 else (0 if self.yVelocity == 0 else -1))
-
-        super().test_grav_line()
         
 
     def render(self, window):
