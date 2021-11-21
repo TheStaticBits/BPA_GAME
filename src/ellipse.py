cdb9387d2d1c4d347a52b7347196127f191ea004
TheStaticBits <TheStaticBits@users.noreply.github.com>
2021-11-21 15:24:15 -0500

    Added animation classes and loading to Ellipse

--- src/ellipse.py
+++ src/ellipse.py
@@ -2,12 +2,19 @@ import pygame
 
 import src.object_base
 import src.constants as constants
+import src.animation
 
 class Ellipse(src.object_base.ObjectBase):
     def __init__(self, startPos, velocity = 0):
         super().__init__(__name__)
 
         self.rect = pygame.Rect(startPos[0], startPos[1], 16, 16)
+
+        self.walkingAnim = src.animation.Animation("res/characters/ellipse/walking.png", 16, 10)
+        self.idleAnim = src.animation.Animation("res/characters/ellipse/idle.png", 16, 10)
+
+        self.playingAnimation = self.idleAnim
+
     
     def update(self, room, inputs):
         super().test_grav_line()
@@ -22,3 +29,11 @@ class Ellipse(src.object_base.ObjectBase):
 
         super().reset_current_tile()
         super().update_y_collision(room)
+
+        self.playingAnimation.update()
+    
+
+    def render(self, window):
+        super().render(window)
+
+        self.playingAnimation.render(window, self.rect.x, self.rect.y)
