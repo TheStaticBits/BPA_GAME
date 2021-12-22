24d330f788b4be14370740878ecd493536a74489
TheStaticBits <50186928+TheStaticBits@users.noreply.github.com>
2021-12-22 13:48:09 -0500

    Commented ellipse_and_corlen.py

--- src/ellipse_and_corlen.py
+++ src/ellipse_and_corlen.py
@@ -1,6 +1,7 @@
 import src.follow_object
 import src.constants as constants
 
+# Creates a follow object with the default Corlen or Ellipse data
 def create_entity(entity, startPos, room, level, velocity = 0) -> "src.follow_object.FollowObject":
     if entity == "ellipse":
         followDist = constants.ELLIPSE_FOLLOW_DISTANCE
