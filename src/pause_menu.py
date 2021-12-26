815d6a035fce6e8d482317db8c755f773bb2c520
TheStaticBits <TheStaticBits@users.noreply.github.com>
2021-12-26 07:52:04 -0500

    Revert "created pause menu transition I think"
    This reverts commit 02b5f28705a6d9975bcd62e199e330b7c7844e20.

--- src/pause_menu.py
+++ src/pause_menu.py
@@ -40,12 +40,6 @@ class PauseMenu(src.scene_base.SceneBase):
             imagePath = constants.PAUSE_BUTTON_PATH,
         )
 
-        # Used for transitions
-        self.transitionPos = -constants.SCREEN_SIZE[1] # For sliding down and up
-        self.transitionMode = "Into"
-        self.transitionAlpha = 255
-        self.transitionSceneTo = None
-
         self.background = None
     
 
@@ -63,38 +57,7 @@ class PauseMenu(src.scene_base.SceneBase):
     # Pass in a surface with the point at which it paused at
     def update_background(self, background):
         self.background = background.copy()
-        # self.background.set_alpha(100) # Alpha
-
-        self.transitionMode = "into" 
-    
-
-    # Transition out of the pause menu
-    def transition_out(self, scene):
-        self.transitionMode = "out"
-        self.transitionSceneTo = scene
-    
-    
-    # Updating transition
-    # Returns true when finishing transitioning out
-    def update_transition(self) ->:
-        if self.transitionMode == "into":
-            self.transitionAlpha -= constants.TRANSITION_SPEED
-            self.transitionPos += constants.SCREEN_SIZE[1] / (155 / constants.TRANSITION_SPEED)
-
-            if self.transitionAlpha <= 100:
-                self.transitionPos = 0
-                self.transitionAlpha = 100
-        
-        elif self.transitionMode == "out":
-            self.transitionAlpha += constants.TRANSITION_SPEED
-            self.transitionPos -= constants.SCREEN_SIZE[1] / (155 / constants.TRANSITION_SPEED)
-
-            if self.transitionAlpha >= 255:
-                self.transitionPos = -constants.SCREEN_SIZE[1]
-                self.transitionAlpha = 255
-                return True
-        
-        return False
+        self.background.set_alpha(100) # Alpha 
     
 
     # Updates the buttons, returning the button pressed
@@ -102,15 +65,12 @@ class PauseMenu(src.scene_base.SceneBase):
     def update(self, inputs, mousePos, mouseInputs) -> str:
         super().update()
 
-        if self.update_transition():
-            return self.transitionSceneTo
-
         for key, button in self.buttons.items():
             if button.update(mousePos, mouseInputs):
                 return key
         
         if inputs["esc"]:
-            self.transition_out("resume")
+            return "resume"
         
         return "pause"
 
@@ -119,15 +79,11 @@ class PauseMenu(src.scene_base.SceneBase):
     def render(self, window):
         super().render()
 
-        # Setting background
-        self.background.set_alpha(self.transitionAlpha)
         window.blit(self.background, (0, 0))
-
-        surf = pygame.Surface(constants.SCREEN_SIZE)
 
         # Rendering buttons
         for button in self.buttons.values():
-            button.render(surf)
+            button.render(window)
         
         # Rendering logo centered on the x axis
-        surf.blit(self.logo, (constants.SCREEN_SIZE[0] / 2 - self.logo.get_width() / 2, 15))
+        window.blit(self.logo, (constants.SCREEN_SIZE[0] / 2 - self.logo.get_width() / 2, 15))
